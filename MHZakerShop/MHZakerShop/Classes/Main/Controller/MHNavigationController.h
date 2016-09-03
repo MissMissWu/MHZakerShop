e65c2bd384e5e6e94124187823f2411112ee2c5b
Mike_He <MH>
2016-09-03 17:56:42 +0800

    修改.gitignore的忽略值
    之前不知道修改了.gitignore文件  导致自己写的东西无法上传，切记不要胡乱修改.gitignore文件

---
+++ MHZakerShop/MHZakerShop/Classes/Main/Controller/MHNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MHNavigationController.h
+//  MHZakerShop
+//
+//  Created by apple on 16/9/3.
+//  Copyright © 2016年 Mike_He. All rights reserved.
+//  导航栏控制器
+
+#import <UIKit/UIKit.h>
+
+@interface MHNavigationController : UINavigationController
+
+@end
---
+++ MHZakerShop/MHZakerShop/Classes/Main/Controller/MHNavigationController.m
@@ -0,0 +1,28 @@
+//
+//  MHNavigationController.m
+//  MHZakerShop
+//
+//  Created by apple on 16/9/3.
+//  Copyright © 2016年 Mike_He. All rights reserved.
+//
+
+#import "MHNavigationController.h"
+
+@interface MHNavigationController ()
+
+@end
+
+@implementation MHNavigationController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
